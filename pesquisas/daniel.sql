c54d07dc1e57bc1b63bcb84a4fa4da923668a9fe
unknown <danielduartee@outlook.com>
2021-10-27 13:20:57 -0300

    segunda pergunta

--- pesquisas/daniel.sql
+++ pesquisas/daniel.sql
@@ -9,3 +9,14 @@ FROM
     episodios
 ORDER BY pontuacao DESC
 LIMIT 1;
+
+-- # 6 - Episódio com maior audiência;
+
+SELECT
+    titulo AS nome_episodio,
+    episodio,
+    us_viewers AS espectadores
+FROM
+    episodios
+ORDER BY us_viewers DESC
+LIMIT 1;
